9b4c81e306f97e1eb4e2397d40212234e182aa23
Seven7Up <seven7up@seven7up.org>
2021-11-14 15:05:28 -0500

    add log header

---
+++ clang_src/log.c
@@ -0,0 +1,151 @@
+#include "log.h"
+#include <stdio.h>
+
+const colors colors_codes = {
+    .BLACK = 30,
+    .RED = 31,
+    .GREEN = 32,
+    .YELLOW = 33,
+    .BLUE = 34,
+    .MAGENTA = 35,
+    .CYAN = 36,
+    .WHITE = 37,
+    .RESET = 39,
+    .LIGHTBLACK_EX = 90,
+    .LIGHTRED_EX = 91,
+    .LIGHTGREEN_EX = 92,
+    .LIGHTYELLOW_EX = 93,
+    .LIGHTBLUE_EX = 94,
+    .LIGHTMAGENTA_EX = 95,
+    .LIGHTCYAN_EX = 96,
+    .LIGHTWHITE_EX = 97,
+};
+
+char *get_color_str(const int code) {
+    char *string = malloc(8);
+    snprintf(string, 8, "\x1b[%dm", code);
+    return string;
+};
+
+// Source: https://www.geeksforgeeks.org/c-program-replace-word-text-another-given-word/
+char *replace_word(const char* s, const char* oldW, const char* newW)
+{
+    char* result;
+    int i, cnt = 0;
+    int newWlen = strlen(newW);
+    int oldWlen = strlen(oldW);
+  
+    // Counting the number of times old word
+    // occur in the string
+    for (i = 0; s[i] != '\0'; i++) {
+        if (strstr(&s[i], oldW) == &s[i]) {
+            cnt++;
+  
+            // Jumping to index after the old word.
+            i += oldWlen - 1;
+        }
+    }
+  
+    // Making new string of enough length
+    result = (char*)malloc(i + cnt * (newWlen - oldWlen) + 1);
+  
+    i = 0;
+    while (*s) {
+        // compare the substring with the result
+        if (strstr(s, oldW) == s) {
+            strcpy(&result[i], newW);
+            i += newWlen;
+            s += oldWlen;
+        }
+        else
+            result[i++] = *s++;
+    }
+  
+    result[i] = '\0';
+    return result;
+}
+
+int info(char *string, ...) {
+    char *code_str = get_color_str(colors_codes.BLUE);
+    char *reset_str = get_color_str(colors_codes.RESET);
+    char *formatted_str = replace_word(string, "\n", "\n    ");
+
+    va_list va_args;
+    va_start(va_args, string);
+    printf("[%s*%s] ", code_str, reset_str);
+    vprintf(formatted_str, va_args);
+    printf("\n");
+    va_end(va_args);
+
+    free(code_str);
+    free(reset_str);
+    return 0;
+};
+
+int succ(char *string, ...) {
+    char *code_str = get_color_str(colors_codes.GREEN);
+    char *reset_str = get_color_str(colors_codes.RESET);
+    char *formatted_str = replace_word(string, "\n", "\n    ");
+
+    va_list va_args;
+    va_start(va_args, string);
+    printf("[%s+%s] ", code_str, reset_str);
+    vprintf(formatted_str, va_args);
+    printf("\n");
+    va_end(va_args);
+    
+    free(code_str);
+    free(reset_str);
+    return 0;
+};
+
+int err(char *string, ...) {
+    char *code_str = get_color_str(colors_codes.RED);
+    char *reset_str = get_color_str(colors_codes.RESET);
+    char *formatted_str = replace_word(string, "\n", "\n    ");
+    
+    va_list va_args;
+    va_start(va_args, string);
+    printf("[%s-%s] ", code_str, reset_str);
+    vprintf(formatted_str, va_args);
+    printf("\n");
+    va_end(va_args);
+    
+    free(code_str);
+    free(reset_str);
+    return 0;
+};
+
+int debug(char *string, ...) {
+    char *code_str = get_color_str(colors_codes.MAGENTA);
+    char *reset_str = get_color_str(colors_codes.RESET);
+    char *formatted_str = replace_word(string, "\n", "\n        ");
+    
+    va_list va_args;
+    va_start(va_args, string);
+    printf("[%sDEBUG%s] ", code_str, reset_str);
+    vprintf(formatted_str, va_args);
+    printf("\n");
+    va_end(va_args);
+    
+    free(code_str);
+    free(reset_str);
+    return 0;
+};
+
+int warn(char *string, ...) {
+    char *code_str = get_color_str(colors_codes.YELLOW);
+    char *reset_str = get_color_str(colors_codes.RESET);
+    char *formatted_str = replace_word(string, "\n", "\n    ");
+    
+    va_list va_args;
+    va_start(va_args, string);
+    printf("[%s!%s] ", code_str, reset_str);
+    vprintf(formatted_str, va_args);
+    printf("\n");
+    va_end(va_args);
+    
+    free(code_str);
+    free(reset_str);
+    return 0;
+};
--- clang_src/log.h
+++ clang_src/log.h
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
 #include <malloc.h>
+
+#ifndef LOG_H
+#define LOG_H
 
 typedef struct {
 const int
@@ -24,142 +27,20 @@ const int
     LIGHTWHITE_EX;
 } colors;
 
-colors colors_codes = {
-    .BLACK = 30,
-    .RED = 31,
-    .GREEN = 32,
-    .YELLOW = 33,
-    .BLUE = 34,
-    .MAGENTA = 35,
-    .CYAN = 36,
-    .WHITE = 37,
-    .RESET = 39,
-    .LIGHTBLACK_EX = 90,
-    .LIGHTRED_EX = 91,
-    .LIGHTGREEN_EX = 92,
-    .LIGHTYELLOW_EX = 93,
-    .LIGHTBLUE_EX = 94,
-    .LIGHTMAGENTA_EX = 95,
-    .LIGHTCYAN_EX = 96,
-    .LIGHTWHITE_EX = 97,
-};
 
-char *get_color_str(const int code) {
-    char *string = malloc(8);
-    snprintf(string, 8, "\x1b[%dm", code);
-    return string;
-};
+char *get_color_str(const int code);
 
 // Source: https://www.geeksforgeeks.org/c-program-replace-word-text-another-given-word/
-char* replace_word(const char* s, const char* oldW,
-                  const char* newW)
-{
-    char* result;
-    int i, cnt = 0;
-    int newWlen = strlen(newW);
-    int oldWlen = strlen(oldW);
-  
-    // Counting the number of times old word
-    // occur in the string
-    for (i = 0; s[i] != '\0'; i++) {
-        if (strstr(&s[i], oldW) == &s[i]) {
-            cnt++;
-  
-            // Jumping to index after the old word.
-            i += oldWlen - 1;
-        }
-    }
-  
-    // Making new string of enough length
-    result = (char*)malloc(i + cnt * (newWlen - oldWlen) + 1);
-  
-    i = 0;
-    while (*s) {
-        // compare the substring with the result
-        if (strstr(s, oldW) == s) {
-            strcpy(&result[i], newW);
-            i += newWlen;
-            s += oldWlen;
-        }
-        else
-            result[i++] = *s++;
-    }
-  
-    result[i] = '\0';
-    return result;
-}
+char *replace_word(const char* s, const char* oldW, const char* newW);
 
-int info(char *string, ...) {
-    char *code_str = get_color_str(colors_codes.BLUE);
-    char *reset_str = get_color_str(colors_codes.RESET);
-    char *formatted_str = replace_word(string, "\n", "\n    ");
+int info(char *string, ...);
 
-    va_list comp;
-    va_start(comp, string);
-    printf("[%s*%s] %s\n", code_str, reset_str, formatted_str, comp);
-    va_end(comp);
+int succ(char *string, ...);
 
-    free(code_str);
-    free(reset_str);
-    return 0;
-};
+int err(char *string, ...);
 
-int succ(char *string, ...) {
-    char *code_str = get_color_str(colors_codes.GREEN);
-    char *reset_str = get_color_str(colors_codes.RESET);
-    char *formatted_str = replace_word(string, "\n", "\n    ");
+int debug(char *string, ...);
 
-    va_list comp;
-    va_start(comp, string);
-    printf("[%s+%s] %s\n", code_str, reset_str, formatted_str, comp);
-    va_end(comp);
-    
-    free(code_str);
-    free(reset_str);
-    return 0;
-};
+int warn(char *string, ...);
 
-int err(char *string, ...) {
-    char *code_str = get_color_str(colors_codes.RED);
-    char *reset_str = get_color_str(colors_codes.RESET);
-    char *formatted_str = replace_word(string, "\n", "\n    ");
-    
-    va_list comp;
-    va_start(comp, string);
-    printf("[%s-%s] %s\n", code_str, reset_str, formatted_str, comp);
-    va_end(comp);
-    
-    free(code_str);
-    free(reset_str);
-    return 0;
-};
-
-int debug(char *string, ...) {
-    char *code_str = get_color_str(colors_codes.MAGENTA);
-    char *reset_str = get_color_str(colors_codes.RESET);
-    char *formatted_str = replace_word(string, "\n", "\n         ");
-    
-    va_list comp;
-    va_start(comp, string);
-    printf("[%sDEBUG%s]: %s\n", code_str, reset_str, formatted_str, comp);
-    va_end(comp);
-    
-    free(code_str);
-    free(reset_str);
-    return 0;
-};
-
-int warn(char *string, ...) {
-    char *code_str = get_color_str(colors_codes.YELLOW);
-    char *reset_str = get_color_str(colors_codes.RESET);
-    char *formatted_str = replace_word(string, "\n", "\n          ");
-    
-    va_list comp;
-    va_start(comp, string);
-    printf("[%sWARNING%s]: %s\n", code_str, reset_str, formatted_str, comp);
-    va_end(comp);
-    
-    free(code_str);
-    free(reset_str);
-    return 0;
-};
+#endif // LOG_H
